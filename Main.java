e57bbfbc28ef4692701e057a94b626df6c99d93d
adil <adilkk58@gmail.com>
2025-05-28 13:49:00 +0530

    created git ignore file and added a code for reverse String

--- Main.java
+++ Main.java
@@ -1,5 +1,21 @@
 public class Main{
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        String str = "echeen araj java";
+        
+        char[] ch = str.toCharArray();
+        int start = 0, end = str.length() - 1;
+
+        while(start < end){
+            // Swap characters at start and end indices
+            char temp = ch[start];
+            ch[start] = ch[end];
+            ch[end] = temp;
+
+            // Move towards the middle
+            start++;
+            end--;
+        }
+
+        System.err.println("Reversed String: " + new String(ch));
     }
 }
