36e30788ec858cc1d963f5799713defa6f039452
adil <adilkk58@gmail.com>
2025-06-01 12:11:15 +0530

    Added comments

--- Main.java
+++ Main.java
@@ -8,5 +8,9 @@ public class Main{
         } else {
             System.out.println("n is odd");
         }
+
+        // This is a simple Java program that checks if a number is even or odd.
+        // The program uses the modulus operator to determine if the number is divisible by 2.
+        // If the number is divisible by 2, it prints "n is even"; otherwise, it prints "n is odd".
     }
 }
