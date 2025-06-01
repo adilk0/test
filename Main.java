88574485782380415eff1b9e2a91aa4fda4e0340
adil <adilkk58@gmail.com>
2025-06-01 12:00:10 +0530

    Change code into odd even number

--- Main.java
+++ Main.java
@@ -1,21 +1,12 @@
+
+
 public class Main{
     public static void main(String[] args) {
-        String str = "echeen araj java";
-        
-        char[] ch = str.toCharArray();
-        int start = 0, end = str.length() - 1;
-
-        while(start < end){
-            // Swap characters at start and end indices
-            char temp = ch[start];
-            ch[start] = ch[end];
-            ch[end] = temp;
-
-            // Move towards the middle
-            start++;
-            end--;
+        int n = 10;
+        if(n % 2 == 0) {
+            System.out.println("n is even");
+        } else {
+            System.out.println("n is odd");
         }
-
-        System.err.println("Reversed String: " + new String(ch));
     }
 }
